76ab86b3b0c334e63c90a636b64ea13f2c75177a
René Dudfield <renesd@gmail.com>
2018-03-28 15:23:56 +0200

    Use TWINE env vars for pypi upload.

--- .travis_osx_upload_whl.py
+++ .travis_osx_upload_whl.py
@@ -7,51 +7,25 @@ import os
 import subprocess
 import sys
 
+def is_master_or_mastertest():
+    cmd = ('git rev-parse --abbrev-ref HEAD')
+    tag_branch = subprocess.check_output(cmd, shell=True)
+    return tag_branch in [b'master\n', b'mastertest\n']
 
-def write_config():
-    """
-    # Set these inside travis. Note, it is on a per repo basis.
-    # https://docs.travis-ci.com/user/environment-variables/#Encrypting-Variables-Using-a-Public-Key
-    # travis encrypt PYPI_USERNAME=super_secret --add
-    # travis encrypt PYPI_PASSWD=super_secret --add
-    """
-    if not os.environ.get('PYPI_USERNAME', None):
-        return
-
-    username = os.environ['PYPI_USERNAME']
-    password = os.environ['PYPI_PASSWD']
-
-    pypirc_template = """\
-[distutils]
-index-servers =
-    pypi
-[pypi]
-repository: https://upload.pypi.io/legacy/
-username: {username}
-password: {password}
-""".format(username=username, password=password)
-
-    with open('pypirc', 'w') as afile:
-        afile.write(pypirc_template)
-
-if '--write-config' in sys.argv:
-    write_config()
-    sys.exit(0)
-else:
-    if '--no-config' not in sys.argv:
-        write_config()
 
 # if --no-git we do not check with git if an upload is needed.
 do_git_check = '--no-git' not in sys.argv
 if do_git_check:
     commit = subprocess.check_output(['git', 'log', '-1'])
     print(commit)
-    if b'UPLOAD' not in commit:
+    if not is_master_or_mastertest() or b'UPLOAD' not in commit:
         print('Not uploading')
         sys.exit(0)
 
 # There should be exactly one .whl
 filenames = glob.glob('dist/*.whl')
+if os.environ.get('UPLOAD_SDIST', None):
+    filenames += glob.glob('dist/*.tar.gz')
 
 
 print('Calling twine to upload...')
